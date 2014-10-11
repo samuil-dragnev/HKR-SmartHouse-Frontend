957f0d37b72c8dce6cf1900be5fc5e9d5f8ca393
samuil-dragnev <samuil.dragnev@gmail.com>
2014-10-11 15:48:15 +0200

    Basic functions
    I've fix a little bit the structure of the files and codes in them. I've
    added a new library to ini a vertical tabs. I've written some Angularjs
    structure code, where I send a request to a local point and retrieve
    JSON information about the house and populate the UI. I've added some
    function for better navigation thru the UI.

---
+++ app/js/house/app.js
@@ -0,0 +1,32 @@
+(function () {
+    var app = angular.module('house-module', []);
+    app.controller('HouseController', [ '$http', '$log', function ($http, $log) {
+        var house = this;
+        this.room = {};
+        this.lastRoom = {};
+        this.setRoom = function (room) {
+            if (jQuery.isEmptyObject(room)) {
+                //Do nothing
+            } else {
+                this.lastRoom = room;
+            }
+            this.room = room;
+        };
+        
+        house.info = [];
+        $http.get("data/house.json").success(function (data) {
+            house.info = data;
+        });
+    } ]);
+    app.controller('PanelController', function () {
+        this.tab = 0;
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+        
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+    
+})();
