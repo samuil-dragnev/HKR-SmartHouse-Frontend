fd1a1e824b3f0e1815501864c322a3e2b86888eb
jollinho <joel.svenssson@live.se>
2015-01-08 15:53:47 +0100

    admin panel
    added functionality for admin panel

--- app/js/house/app.js
+++ app/js/house/app.js
@@ -18,6 +18,7 @@
         ]
     );
     app.controller('HouseController', ['$http', '$scope', function ($http, $scope) {
+        $scope.isSwitched = false;
         $scope.room = {};
         $scope.lastRoom = {};
         $scope.setRoom = function (room) {
@@ -26,6 +27,9 @@
             }
             $scope.room = room;
         };
+        $scope.showAdmin = function() {
+            $scope.isSwitched = true;
+        }
         $scope.house = [];
         $http.get("data//house.json").success(function (data) {
             $scope.house = data;
@@ -33,10 +37,51 @@
         $scope.tab = 0;
         $scope.selectTab = function (setTab) {
             $scope.tab = setTab;
+            $scope.isSwitched = false;
         };
 
         $scope.isSelected = function (checkTab) {
             return $scope.tab === checkTab;
+        };
+        $scope.getAllRooms = function() {
+            //rooms is a object transfer it into an array
+            var rooms = $scope.house[3].rooms;
+            var roomsArray = [];
+            for(var i = 0; i < rooms.length; i ++){
+                roomsArray.push(rooms[i]);
+            }
+            return roomsArray;
+//            var arr = [];
+//            var arr1 = [];
+//            for( var i in rooms ) {
+//                if (rooms.hasOwnProperty(i)){
+//                    arr.push(rooms[i]);
+//                    
+//                    //console.log(arr[i]);
+//                }
+//                
+//            }
+//            for( var j in arr ) {
+//                if (arr[j].hasOwnProperty(j)){
+//                    arr1.push(arr[j].devices);
+//
+//                    console.log(arr1[j].name);
+//                }
+//
+//            }
+//            
+//            return rooms;
+//            var allDevices = [];
+//            var rooms = $scope.house[3].rooms;
+//            for(var j = 0; j < rooms.length; j ++) {
+//                for(var i = 0; i < rooms.devices.length; i ++){
+//                    allDevices[i] = rooms.devices[i];
+//                    
+//                    console.log(room.devices[i]);
+//                    
+//                }
+//            }
+//            return allDevices;
         };
     }]);
 
